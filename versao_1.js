d6e0eb107ed3acffd67463b07066eb25a7c0541d
Eduardo Carvalho <e.alves.rj@outlook.com>
2023-06-14 16:54:09 -0300

    projeto finalizado para correção

--- versao_1.js
+++ versao_1.js
@@ -10,33 +10,39 @@
  * 
  * 
  */
-console.log("Boas vindas ao jogo de Blackjack!");
+   //  console.log("Boas vindas ao jogo de Blackjack!");
 
-let iniciarRodada = confirm("Quer iniciar uma nova rodada?");
-
-if (iniciarRodada) {
-   let usuario = comprarCarta().valor + comprarCarta().valor;
-   let computador = comprarCarta().valor + comprarCarta().valor;
-
-   console.log(`Usuário - Cartas: ${comprarCarta().texto}, ${comprarCarta().texto} - Valor: ${usuario}`);
-   console.log(`Computador - Cartas: ${comprarCarta().texto}, ${comprarCarta().texto} - Valor: ${computador}`);
-
-   if (usuario === computador) {
-      console.log("Empate.");
-   } else if (usuario > computador && usuario <= 21) {
-      console.log("O usuário ganhou.");
-   } else if (computador > usuario && computador <= 21) {
-      console.log("O computador ganhou.");
-   } else if (usuario > 21 && computador <= 21) {
-      console.log("O computador ganhou.");
-   } else if (computador > 21 && usuario <= 21) {
-      console.log("O usuário ganhou.");
-   } else {
-      console.log("Ambos perderam.");
-   }
-} else {
-   console.log("O jogo acabou.");
-}
+   //  let iniciarRodada = confirm("Quer iniciar uma nova rodada?");
+    
+   //  if (iniciarRodada) {
+   //     let cartaUsuario1 = comprarCarta();
+   //     let cartaUsuario2 = comprarCarta();
+   //     let cartaComputador1 = comprarCarta();
+   //     let cartaComputador2 = comprarCarta();
+    
+   //     let usuario = cartaUsuario1.valor + cartaUsuario2.valor;
+   //     let computador = cartaComputador1.valor + cartaComputador2.valor;
+    
+   //     console.log(`Usuário - Cartas: ${cartaUsuario1.texto}, ${cartaUsuario2.texto} - Valor: ${usuario}`);
+   //     console.log(`Computador - Cartas: ${cartaComputador1.texto}, ${cartaComputador2.texto} - Valor: ${computador}`);
+    
+   //     if (usuario === computador) {
+   //        console.log("Empate.");
+   //     } else if (usuario > computador && usuario <= 21) {
+   //        console.log("O usuário ganhou.");
+   //     } else if (computador > usuario && computador <= 21) {
+   //        console.log("O computador ganhou.");
+   //     } else if (usuario > 21 && computador <= 21) {
+   //        console.log("O computador ganhou.");
+   //     } else if (computador > 21 && usuario <= 21) {
+   //        console.log("O usuário ganhou.");
+   //     } else {
+   //        console.log("Ambos perderam.");
+   //     }
+   //  } else {
+   //     console.log("O jogo acabou.");
+   //  }
+    
 
 
 
